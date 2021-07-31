8a32f397867a15726929c2008f7ee268ae78aa55
SANTANYN <willgh0st@mail.ru>
2021-08-01 00:06:42 +0300

    Добавил пеезод между страницами в test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -18,6 +18,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    time.sleep(5)
+    login_page.should_be_login_url()
 
 # def test_guest_can_go_to_login_page(browser):
 #     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
